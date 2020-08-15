09e6805c688296d4f6ca71bb74a84aa56675c709
stembe <stembe@student.wethinkcode.co.za>
2020-08-15 14:33:37 +0200

    add game assets

--- game.py
+++ game.py
@@ -1,14 +1,22 @@
+#import modules
 import pygame
-
-pygame.init
-
+# display game main menu
+def renderMainMenu():
+    pass
+# start game
+def renderGame():
+    pass
+def SpawnCharacter():
+    pass
+#initialize game
+pygame.init()
+#screen size
+screenWidth=800
+screenhieght=400
 running = True
-
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT
-            running False
-        
-        screen.fill((255, 255, 255))
-
+        if event.type == pygame.QUIT:
+            running = False
+        pygame.display.set_mode((screenWidth, screenHeight))
 pygame.quit
