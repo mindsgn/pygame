7a06105811a53e5b7da7320e9e114235e3b466a4
stembe <stembe@student.wethinkcode.co.za>
2020-08-15 14:12:18 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT
+            running False
+        
+        screen.fill((255, 255, 255))
+
+pygame.quit
